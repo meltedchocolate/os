f5a48e0e8d28ce6f8c188c41cf8fffd794bc1abf
Ricky LeDew <adotout1@gmail.com>
2016-04-10 17:14:00 -0400

    fixed print routine

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,26 +1,31 @@
-[org 0x7c00]
+
 	
 mov ah, 0x0e
 
 mov bp, 0x8000			;Set up stack, with enough room to grow downwards
 mov sp, bp
 
-mov bx, STRING
-call print
+mov al, [ the_secret ]
+int 0x10 					; Does this print an X?
+mov bx, 0x7c0				; Can ’t set ds directly , so set bx
+mov ds, bx 				; then copy bx to ds.
+mov al, [ the_secret ]
+int 0x10 					; Does this print an X?
+mov al, [es: the_secret ] 	; Tell the CPU to use the es ( not ds) segment.
+int 0x10 					; Does this print an X?
+mov bx, 0x7c0
+mov es, bx
+mov al, [es: the_secret ]
+int 0x10 					; Does this print an X?
 
-mov bx, SECOND_STRING
-call print
 	
-jmp $					;Jump to current position and loop forever
+jmp $						;Jump to current position and loop forever
 
 %include "print.asm"
 
 ;Data
-STRING:
-	db 'hello, world! ', 0
-	
-SECOND_STRING:
-	db 'THIS ACTUALLY WORKS WHAT ', 0
+the_secret:
+	db "x"
 	
 times 510-($-$$) db 0	;Repeat 0x00 510-(the amount of space already used)
 						; to pad the file with 0s until the magic number
