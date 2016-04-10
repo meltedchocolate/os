d2a854eb4991a9832c186a100a183836ae723ee8
Ricky LeDew <adotout1@gmail.com>
2016-04-10 09:57:15 -0400

    Added stack

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,22 +1,28 @@
 org 0x7c00
+	
+mov ah, 0x0e
 
-print:
-	mov ah, 0x0e			;Set bios routine to printing characters
-	mov al, character
-	int 0x10
-	mov al, [character]
-	int 0x10
-	mov bx, character
-	add bx, 0x7c00
-	mov al, [bx]
-	int 0x10
-	mov al, [0x7c1e]
-	int 0x10
+mov bp, 0x8000			;Set up stack, with enough room to grow downwards
+mov sp, bp
+
+push 'A'				;Push data onto stack
+push 'B'
+push 'C'
+
+pop bx					;Pop data off stack and move the low half of bx (bl) to al,
+mov al, bl				; since al is 8 bits (for ascii)
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
 	
 jmp $					;Jump to current position and loop forever
-
-character:
-	db "X"
 
 times 510-($-$$) db 0	;Repeat 0x00 510-(the amount of space already used)
 						; to pad the file with 0s until the magic number
